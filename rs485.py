12aadab9d25bc9f418dffe8be4e815d19eb846c3
HoaiMy286 <my.nguyenmy@hcmut.edu.vn>
2024-06-12 22:05:49 +0700

    update rs485

--- rs485.py
+++ rs485.py
@@ -30,8 +30,7 @@ def serial_read_data(ser, expected_id=None, max_attempts=5):
         if bytesToRead > 0:
             out = ser.read(bytesToRead)
             data_array = [b for b in out]
-            print(attempts)
-            print("Data received: {data_array}")
+            print(f"Attempt {attempts}, Data received: {data_array}")
             if len(data_array) >= 7:
                 array_size = len(data_array)
                 value = data_array[array_size - 4] * 256 + data_array[array_size - 3]
@@ -45,12 +44,16 @@ def serial_read_data(ser, expected_id=None, max_attempts=5):
     print("Max attempts reached, did not receive expected ID")
     return -1
 
+def clear_buffer(ser):
+    while ser.inWaiting() > 0:
+        ser.read(ser.inWaiting())
+
 # ==================================================
 # =========     TEMP          ======================
 
 soil_temperature = [10, 3, 0, 6, 0, 1, 101, 112]
 def readTemperature():
-    serial_read_data(ser)  # Xóa dữ liệu cũ
+    clear_buffer(ser)  # Xóa dữ liệu cũ
     ser.write(soil_temperature)
     time.sleep(1)
     return serial_read_data(ser, expected_id=10) / 100
@@ -60,7 +63,7 @@ def readTemperature():
 
 soil_moisture = [10, 3, 0, 7, 0, 1, 52, 176]
 def readMoisture():
-    serial_read_data(ser)  # Xóa dữ liệu cũ
+    clear_buffer(ser)  # Xóa dữ liệu cũ
     ser.write(soil_moisture)
     time.sleep(1)
     return serial_read_data(ser, expected_id=10) / 100
@@ -72,8 +75,8 @@ mixer1_ON  = [1, 6, 0, 0, 0, 255, 201, 138]
 mixer1_OFF = [1, 6, 0, 0, 0, 0, 137, 202]
 
 def setMixer1(state):
-    serial_read_data(ser)  # Xóa dữ liệu cũ
-    if state == True:
+    clear_buffer(ser)  # Xóa dữ liệu cũ
+    if state:
         ser.write(mixer1_ON)
     else:
         ser.write(mixer1_OFF)
@@ -87,8 +90,8 @@ mixer2_ON  = [2, 6, 0, 0, 0, 255, 201, 185]
 mixer2_OFF = [2, 6, 0, 0, 0, 0, 137, 249]
 
 def setMixer2(state):
-    serial_read_data(ser)  # Xóa dữ liệu cũ
-    if state == True:
+    clear_buffer(ser)  # Xóa dữ liệu cũ
+    if state:
         ser.write(mixer2_ON)
     else:
         ser.write(mixer2_OFF)
@@ -102,8 +105,8 @@ mixer3_ON  = [3, 6, 0, 0, 0, 255, 200, 104]
 mixer3_OFF = [3, 6, 0, 0, 0, 0, 136, 40]
 
 def setMixer3(state):
-    serial_read_data(ser)  # Xóa dữ liệu cũ
-    if state == True:
+    clear_buffer(ser)  # Xóa dữ liệu cũ
+    if state:
         ser.write(mixer3_ON)
     else:
         ser.write(mixer3_OFF)
@@ -117,8 +120,8 @@ selector4_ON  = [4, 6, 0, 0, 0, 255, 201, 223]
 selector4_OFF = [4, 6, 0, 0, 0, 0, 137, 159]
 
 def setSelector4(state):
-    serial_read_data(ser)  # Xóa dữ liệu cũ
-    if state == True:
+    clear_buffer(ser)  # Xóa dữ liệu cũ
+    if state:
         ser.write(selector4_ON)
     else:
         ser.write(selector4_OFF)
@@ -132,8 +135,8 @@ selector5_ON  = [5, 6, 0, 0, 0, 255, 200, 14]
 selector5_OFF = [5, 6, 0, 0, 0, 0, 136, 78]
 
 def setSelector5(state):
-    serial_read_data(ser)  # Xóa dữ liệu cũ
-    if state == True:
+    clear_buffer(ser)  # Xóa dữ liệu cũ
+    if state:
         ser.write(selector5_ON)
     else:
         ser.write(selector5_OFF)
@@ -147,8 +150,8 @@ selector6_ON  = [6, 6, 0, 0, 0, 255, 200, 61]
 selector6_OFF = [6, 6, 0, 0, 0, 0, 136, 125]
 
 def setSelector6(state):
-    serial_read_data(ser)  # Xóa dữ liệu cũ
-    if state == True:
+    clear_buffer(ser)  # Xóa dữ liệu cũ
+    if state:
         ser.write(selector6_ON)
     else:
         ser.write(selector6_OFF)
@@ -162,8 +165,8 @@ pumpin_ON  = [7, 6, 0, 0, 0, 255, 201, 236]
 pumpin_OFF = [7, 6, 0, 0, 0, 0, 137, 172]
 
 def setPumpIn(state):
-    serial_read_data(ser)  # Xóa dữ liệu cũ
-    if state == True:
+    clear_buffer(ser)  # Xóa dữ liệu cũ
+    if state:
         ser.write(pumpin_ON)
     else:
         ser.write(pumpin_OFF)
@@ -177,14 +180,15 @@ pumpout_ON  = [8, 6, 0, 0, 0, 255, 201, 19]
 pumpout_OFF = [8, 6, 0, 0, 0, 0, 137, 83]
 
 def setPumpOut(state):
-    serial_read_data(ser)  # Xóa dữ liệu cũ
-    if state == True:
+    clear_buffer(ser)  # Xóa dữ liệu cũ
+    if state:
         ser.write(pumpout_ON)
     else:
         ser.write(pumpout_OFF)
     time.sleep(1)
     print(serial_read_data(ser, expected_id=8))
 
+# Test
 while True:
     print("MIXER 1")
     setMixer1(True)
