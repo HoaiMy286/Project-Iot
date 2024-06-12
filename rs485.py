1be4ebc7a608f4ee3eb06491278cf80df3fc9de6
HoaiMy286 <my.nguyenmy@hcmut.edu.vn>
2024-06-12 21:40:05 +0700

    update rs485

--- rs485.py
+++ rs485.py
@@ -24,8 +24,7 @@ try:
 except:
     print("Can not open the port")
 
-# đọc dữ liệu từ cổng nối tiếp, xử lý nó và trả về một giá trị cụ thể.
-def serial_read_data(ser):
+def serial_read_data(ser, expected_id=None):
     bytesToRead = ser.inWaiting()
     if bytesToRead > 0:
         out = ser.read(bytesToRead)
@@ -34,7 +33,15 @@ def serial_read_data(ser):
         if len(data_array) >= 7:
             array_size = len(data_array)
             value = data_array[array_size - 4] * 256 + data_array[array_size - 3]
-            return value
+
+            if expected_id is not None:
+                if data_array[0] == expected_id:
+                    return value
+                else:
+                    print("Unexpected ID: ", data_array[0])
+                    return -1
+            else:
+                return value
         else:
             return -1
     return 0
@@ -44,15 +51,10 @@ def serial_read_data(ser):
 
 soil_temperature = [10, 3, 0, 6, 0, 1, 101, 112]
 def readTemperature():
-    # kiểm tra và xóa bộ đệm dữ liệu cũ (nếu có) 
-    # trước khi gửi yêu cầu mới.
     serial_read_data(ser)
-    # yêu cầu đọc giá trị nhiệt độ từ thiết bị Modbus.
     ser.write(soil_temperature)
     time.sleep(1)
-    # đọc dữ liệu trả về từ thiết bị. 
-    # Giá trị trả về từ hàm này sẽ là kết quả đọc được từ thiết bị.
-    return serial_read_data(ser)/100
+    return serial_read_data(ser, expected_id=10) / 100
 
 # ==================================================
 # =========     SOIL MOISTURE        ===============
@@ -62,7 +64,7 @@ def readMoisture():
     serial_read_data(ser)
     ser.write(soil_moisture)
     time.sleep(1)
-    return serial_read_data(ser)/100
+    return serial_read_data(ser, expected_id=10) / 100
 
 # ==================================================
 # =========     FLOW SENSOR       ==================
@@ -72,7 +74,7 @@ def readFlow():
     # serial_read_data(ser)
     # ser.write(flow_sensor)
     # time.sleep(1)
-    # return serial_read_data(ser)
+    # return serial_read_data(ser, expected_id=1)
     return 40
 
 # ==================================================
@@ -83,7 +85,7 @@ def readSonar():
     # serial_read_data(ser)
     # ser.write(sonar_sensor)
     # time.sleep(1)
-    # return serial_read_data(ser)
+    # return serial_read_data(ser, expected_id=1)
     return 1
 
 # ==================================================
@@ -98,7 +100,7 @@ def setMixer1(state):
     else:
         ser.write(mixer1_OFF)
     time.sleep(1)
-    print(serial_read_data(ser))
+    print(serial_read_data(ser, expected_id=1))
 
 # ==================================================
 # =========     MIXER 2        =====================
@@ -112,7 +114,7 @@ def setMixer2(state):
     else:
         ser.write(mixer2_OFF)
     time.sleep(1)
-    print(serial_read_data(ser))
+    print(serial_read_data(ser, expected_id=2))
 
 # ==================================================
 # =========     MIXER 3        =====================
@@ -126,7 +128,7 @@ def setMixer3(state):
     else:
         ser.write(mixer3_OFF)
     time.sleep(1)
-    print(serial_read_data(ser))
+    print(serial_read_data(ser, expected_id=3))
 
 # ==================================================
 # =========     SELECTOR 4        ==================
@@ -140,7 +142,7 @@ def setSelector4(state):
     else:
         ser.write(selector4_OFF)
     time.sleep(1)
-    print(serial_read_data(ser))
+    print(serial_read_data(ser, expected_id=4))
 
 # ==================================================
 # =========     SELECTOR 5        ==================
@@ -154,7 +156,7 @@ def setSelector5(state):
     else:
         ser.write(selector5_OFF)
     time.sleep(1)
-    print(serial_read_data(ser))
+    print(serial_read_data(ser, expected_id=5))
 
 # ==================================================
 # =========     SELECTOR 5        ==================
@@ -168,7 +170,7 @@ def setSelector6(state):
     else:
         ser.write(selector6_OFF)
     time.sleep(1)
-    print(serial_read_data(ser))
+    print(serial_read_data(ser, expected_id=6))
 
 # ==================================================
 # =========     PUMP IN 7         ==================
@@ -182,7 +184,7 @@ def setPumpIn(state):
     else:
         ser.write(pumpin_OFF)
     time.sleep(1)
-    print(serial_read_data(ser))
+    print(serial_read_data(ser, expected_id=7))
 
 # ==================================================
 # =========     PUMP OUT 8        ==================
@@ -196,14 +198,14 @@ def setPumpOut(state):
     else:
         ser.write(pumpout_OFF)
     time.sleep(1)
-    print(serial_read_data(ser))
+    print(serial_read_data(ser, expected_id=8))
 
-# while True:
-#     print("MIXER 1")
-#     setMixer1(True)
-#     time.sleep(2)
-#     setMixer1(False)
-#     time.sleep(2)
+while True:
+    print("MIXER 1")
+    setMixer1(True)
+    time.sleep(2)
+    setMixer1(False)
+    time.sleep(2)
 
 #     print("soil moisture:")
 #     print(readMoisture())
@@ -213,11 +215,11 @@ def setPumpOut(state):
 #     print(readTemperature())
 #     time.sleep(2)
 
-#     print("MIXER 2")
-#     setMixer2(True)
-#     time.sleep(2)
-#     setMixer2(False)
-#     time.sleep(2)
+    print("MIXER 2")
+    setMixer2(True)
+    time.sleep(2)
+    setMixer2(False)
+    time.sleep(2)
 
 #     print("MIXER 3")
 #     setMixer3(True)
@@ -251,18 +253,4 @@ def setPumpOut(state):
 
 #     print("PUMP OUT")
 #     setPumpOut(True)
-#     time.sleep(2)
-#     setPumpOut(False)
-#     time.sleep(2)
-
-
-# while True:
-#     print("TEST SENSOR")
-
-# #     print("flow:")
-# #     print(readFlow())
-# #     time.sleep(2)
-
-# #     print("sonar:")
-# #     print(readSonar())
-# #     time.sleep(2)
+#     time.sleep(
